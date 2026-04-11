1eb28edcf29a083be318dc555ae56506dc027c64
MohamedRedha_ABDERRAHMANE <chbmy2003@gmail.com>
2026-04-12 00:43:06 +0200

    linear stage header file changes  from Axis_3D_1.0 didn't get passed to this branch. Copied the changes.

--- include/linearStage.hpp
+++ include/linearStage.hpp
@@ -1,4 +1,4 @@
-
+﻿
 
 #ifndef LINEARSTAGE_H
 
@@ -33,11 +33,15 @@ private:
     static constexpr int VELOCITYTYPE = 1;
     static constexpr int ACCELERATIONTYPE = 2;
 
+    static constexpr double COUNTS_PER_MM = 34554.96;  // MTSZ-B8 encoder counts per mm
+    static constexpr double COUNTS_PER_MM_VEL = 772981.4;  // velocity scaling (counts/s)
+    static constexpr double COUNTS_PER_MM_ACC = 261.93;    // acceleration scaling
 
-    const double defaultVelocityRU = 0.9; //2.0;
-    const double defaultAccelerationRU = 0.2; //1.0;
 
-    int serialNumber=0;
+    const double defaultVelocityRU = 2.0; //2.0;
+    const double defaultAccelerationRU = 100.0; //1.0;
+
+    int serialNumber = 0;
 
 
     int positionDU = 346020;  // 1mm ->34602 device units 
@@ -50,7 +54,7 @@ private:
     int accelerationDU = 0;
 
     double velocityRU = 2;
-    double accelerationRU = 1;
+    double accelerationRU = 100;
 
     char bufferSerialNo[16] = {};
     char bufferSerialNoList[100] = {};
@@ -73,7 +77,7 @@ private:
 
 
 public:
-    LinearStage(const int serialnumber = DEFAULTSERIALNUMBER) : serialNumber(serialnumber) { std::cout << "serial number " << serialNumber<<std::endl; }
+    LinearStage(const int serialnumber = DEFAULTSERIALNUMBER) : serialNumber(serialnumber) { std::cout << "serial number " << serialNumber << std::endl; }
 
 
     ~LinearStage() {
@@ -93,19 +97,18 @@ public:
             return true;
         }
 
-        sprintf_s(bufferSerialNo, "%d", serialNumber); // assign to bufferSerialNo formatted version of the Serial number
+        sprintf_s(bufferSerialNo, "%d", serialNumber);
         if (TLI_BuildDeviceList() != SUCCESS) {
             std::cout << "Failed to fetch device list \n";
             return false;
-
         }
+
         deviceSerialNo = TLI_GetDeviceListSize();
 
         if (TLI_GetDeviceListByTypeExt(bufferSerialNoList, BUFFERSIZE, TYPEID) != SUCCESS) {
             std::cout << "Failed to get the content of device list\n";
             return false;
         }
-
 
         if (!strstr(bufferSerialNoList, bufferSerialNo)) {
             std::cout << "Failed to find specified serial number in list\n";
@@ -118,32 +121,34 @@ public:
         }
 
         if (!CC_StartPolling(bufferSerialNo, POLLINGRATE)) {
-            std::cout << "Failed to start polling to device \n";
+            std::cout << "Failed to start polling\n";
             return false;
         }
 
-        if (!DeviceConnected()) {
-            std::cout << "Device not connected\n";
-            return false;
-        }
+        Sleep(1000); // DC servo needs longer init time than stepper
         CC_ClearMessageQueue(bufferSerialNo);
 
-        if (!CC_LoadSettings(bufferSerialNo)) {
-            std::cout << "Failed to load settings from device\n";
+        if (CC_RequestSettings(bufferSerialNo) != SUCCESS) { // Request device state
+            std::cout << "Could not download settings from device\n";
             return false;
         }
+
+        Sleep(400);
+
+        if (!CC_LoadSettings(bufferSerialNo)) {
+            std::cout << "Warning: CC_LoadSettings failed, using hardcoded MTSZ-B8 params\n";
+            return false;
+        }
+
+        CC_SetLimitsSoftwareApproachPolicy(bufferSerialNo, AllowAllMoves);
 
         if (!SetVelocityAccelerationParams(defaultAccelerationRU, defaultVelocityRU)) {
             std::cout << "Failed to set default velocity and acceleration settings\n";
             return false;
-
         }
-        CC_SetLimitsSoftwareApproachPolicy(bufferSerialNo, AllowAllMoves); // To make the linear stage move in the negative range
-        
+
         std::cout << "Linear stage connected and ready to use\n";
         return true;
-
-
     }
 
 
@@ -233,20 +238,21 @@ public:
 
     // ADD IN GUI 
     bool SetVelocityAccelerationParams(const double accelerationInMm, const double velocityInMm) {
+        velocityDU = (int)(velocityInMm * COUNTS_PER_MM_VEL);
+        accelerationDU = (int)(accelerationInMm * COUNTS_PER_MM_ACC);
 
-        if (CC_GetDeviceUnitFromRealValue(bufferSerialNo, velocityInMm, &velocityDU, VELOCITYTYPE) != SUCCESS || CC_GetDeviceUnitFromRealValue(bufferSerialNo, accelerationInMm, &accelerationDU, ACCELERATIONTYPE) != SUCCESS) {
-            std::cout << "Failed to convert real values to device values\n";
+        std::cout << "Serial " << serialNumber
+            << " | velDU: " << velocityDU
+            << " | accDU: " << accelerationDU << "\n";
+
+        if (CC_SetVelParams(bufferSerialNo, accelerationDU, velocityDU) != SUCCESS) {
+            std::cout << "Failed to set velocity/acceleration params\n";
             return false;
         }
-        std::cout << "acceleration: " << accelerationDU << " velocity: " << velocityDU << "\n";
 
-        if (CC_SetVelParams(bufferSerialNo, accelerationDU, velocityDU) == SUCCESS) {
-            accelerationRU = accelerationInMm;
-            velocityRU = velocityInMm;
-            return true;
-        }
-        return false;
-
+        velocityRU = velocityInMm;
+        accelerationRU = accelerationInMm;
+        return true;
     }
 
     bool DeviceConnected() const {
@@ -257,7 +263,7 @@ public:
 
 
 
-private:
+
 
 
 
